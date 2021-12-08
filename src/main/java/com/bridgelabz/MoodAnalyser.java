df7d72c21c4dabc8dc622c88fd61778abd85ff85
ipratikner <pratiknerlikar@gmail.com>
2021-12-08 22:54:37 +0530

    Added: refactor mood analyze test file main file

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,12 +1,27 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    public MoodAnalyser(String message) {
+    public String message;
+    //constructor of class
+    public MoodAnalyser() {
     }
-    public String analyseMood(String message){
-        if (message.contains("SAD"))
-            return "SAD";
-        else
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+    //analyseMood instance method
+    public String analyseMood(String message) {
+        this.message = message;
+        return analyseMood();
+    }
+    //analyseMood method
+    public String analyseMood() {
+        try {
+            if (message.contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }catch (NullPointerException e){
             return "HAPPY";
+        }
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -20,5 +20,18 @@ class MoodAnalyserTest {
         String mood = moodAnalyser.analyseMood("I am in  happy Mood ");
         Assertions.assertEquals("HAPPY",mood);
     }
-
+    //uc1 Refactor TC1.1
+    @Test
+    public void givenMessage_Constructor_WhenSad_ShouldReturnSad(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood ");
+        String mood = moodAnalyser.analyseMood("SAD");
+        Assertions.assertEquals("SAD",mood);
+    }
+    //uc1 Refactor TC1.2
+    @Test
+    public void givenMessagen_Constructor_WhenNotSad_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in  happy Mood ");
+        String mood = moodAnalyser.analyseMood("Happy");
+        Assertions.assertEquals("HAPPY",mood);
+    }
 }
